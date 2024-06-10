653790245a8bf3d7d6f3c327044deae7c32c7f03
JoBarMa <josepbaradat@gmail.com>
2024-06-10 21:00:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.utils
 import torch.nn as nn
 from torchvision import models
+from collections import defaultdict
+import torch.nn.functional as F
+from loss import dice_loss
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import time
+import copy
+from tqdm import tqdm
 
 # Load the images and masks
 imgs_train_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/Train/imgs_train.npy'
-imgs_test_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/Test/imgs_test.npy'
+imgs_val_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/Val/imgs_val.npy'
 masks_train_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/Train/masks_train.npy'
-masks_test_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/Test/masks_test.npy'
+masks_val_path = '/Users/josep/Desktop/aidl-2024-spring-mlops/BCI/PanNuke_dataset/FinalData/val/masks_val.npy'
 
 # Load the data from the .npy files
 imgs_train = np.load(imgs_train_path)
-imgs_test = np.load(imgs_test_path)
+imgs_val = np.load(imgs_val_path)
 masks_train = np.load(masks_train_path)
-masks_test = np.load(masks_test_path)
+masks_val = np.load(masks_val_path)
 
 # Extract the last channel of the masks
 masks_train = masks_train[:, :, :, -1]
-masks_test = masks_test[:, :, :, -1]
+masks_val = masks_val[:, :, :, -1]
 
 #first_index_train = 0
 #first_index_test = 0
@@ -85,14 +93,14 @@ class SimDataset(Dataset):
 
 # Create the datasets
 train_set = SimDataset(imgs_train, masks_train, transform=trans)
-test_set = SimDataset(imgs_test, masks_test, transform=trans)
+val_set = SimDataset(imgs_val, masks_val, transform=trans)
 
 # Create the dataloaders
 batch_size = 25
 
 dataloaders = {
     'train': DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0),
-    'test': DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0)
+    'val': DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=0) 
 }
 
 # Function to reverse the normalization
@@ -149,10 +157,10 @@ def convrelu(in_channels, out_channels, kernel, padding):
     )
 
 class ResNetUNet(nn.Module):
-    def __init__(self, n_class):
+    def __init__(self, n_class=1): #I punt =1 here because by default we will only take into account one type of masks
         super().__init__()
 
-        self.base_model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.base_model = models.resnet18(weights='ResNet18_Weights.DEFAULT')
         self.base_layers = list(self.base_model.children())
 
         self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
@@ -181,10 +189,10 @@ class ResNetUNet(nn.Module):
         self.conv_last = nn.Conv2d(64, n_class, 1)
 
     def forward(self, input):
-        x_original = self.conv_original_size0(input)
+        x_original = self.conv_original_size0(input.float()) #We convert the input to float
         x_original = self.conv_original_size1(x_original)
 
-        layer0 = self.layer0(input)
+        layer0 = self.layer0(input.float()) #We convert the input to float
         layer1 = self.layer1(layer0)
         layer2 = self.layer2(layer1)
         layer3 = self.layer3(layer2)
@@ -219,10 +227,185 @@ class ResNetUNet(nn.Module):
 
         return out
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = ResNetUNet(n_class=6)
-model = model.to(device)
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#model = ResNetUNet(n_class=1)
+#model = model.to(device)
 
 # check keras-like model summary using torchsummary
-from torchsummary import summary
-summary(model, input_size=(3, 224, 224))
+#from torchsummary import summary
+#summary(model, input_size=(3, 224, 224))
+
+def calc_loss(pred, target, metrics, bce_weight=0.5):
+    target = target.unsqueeze(1) #Extra dimension added to match dimensions
+
+    bce = F.binary_cross_entropy_with_logits(pred, target)
+
+    pred = F.sigmoid(pred)
+    dice = dice_loss(pred, target)
+
+    loss = bce * bce_weight + dice * (1 - bce_weight)
+
+    metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
+    metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+    metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
+
+    return loss
+
+def print_metrics(metrics, epoch_samples, phase):
+    outputs = []
+    for k in metrics.keys():
+        outputs.append("{}: {:4f}".format(k, metrics[k] / epoch_samples))
+
+    print("{}: {}".format(phase, ", ".join(outputs)))
+
+#def train_model(model, optimizer, scheduler, num_epochs=25):
+#    best_model_wts = copy.deepcopy(model.state_dict())
+#    best_loss = 1e10
+#
+#    for epoch in range(num_epochs):
+#        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+#        print('-' * 10)
+#
+#        since = time.time()
+#
+#        # Each epoch has a training and validation phase
+#        for phase in ['train', 'val']:
+#            if phase == 'train':
+#                scheduler.step()
+#                for param_group in optimizer.param_groups:
+#                    print("LR", param_group['lr'])
+#
+#                model.train()  # Set model to training mode
+#            else:
+#                model.eval()   # Set model to evaluate mode
+#
+#            metrics = defaultdict(float)
+#            epoch_samples = 0
+#
+#            for inputs, labels in dataloaders[phase]:
+#                inputs = inputs.to(device)
+#                labels = labels.to(device)
+#
+#                # zero the parameter gradients
+#                optimizer.zero_grad()
+#
+#                # forward
+#                # track history if only in train
+#                with torch.set_grad_enabled(phase == 'train'):
+#                    outputs = model(inputs)
+#                    loss = calc_loss(outputs, labels, metrics)
+#
+#                    # backward + optimize only if in training phase
+#                    if phase == 'train':
+#                        loss.backward()
+#                        optimizer.step()
+#
+#                # statistics
+#                epoch_samples += inputs.size(0)
+#
+#            print_metrics(metrics, epoch_samples, phase)
+#            epoch_loss = metrics['loss'] / epoch_samples
+#
+#            # deep copy the model
+#            if phase == 'val' and epoch_loss < best_loss:
+#                print("saving best model")
+#                best_loss = epoch_loss
+#                best_model_wts = copy.deepcopy(model.state_dict())
+#
+#        time_elapsed = time.time() - since
+#        print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+#
+#    print('Best val loss: {:4f}'.format(best_loss))
+#
+#    # load best model weights
+#    model.load_state_dict(best_model_wts)
+#    return model
+
+def train_model(model, optimizer, scheduler, num_epochs=25):
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 1e10
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        since = time.time()
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                for param_group in optimizer.param_groups:
+                    print("LR", param_group['lr'])
+
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            metrics = defaultdict(float)
+            epoch_samples = 0
+
+            # Wrap the dataloader with tqdm for the progress bar
+            for inputs, labels in tqdm(dataloaders[phase], desc=f"{phase} epoch {epoch}/{num_epochs - 1}"):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = calc_loss(outputs, labels, metrics)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                epoch_samples += inputs.size(0)
+
+            print_metrics(metrics, epoch_samples, phase)
+            epoch_loss = metrics['loss'] / epoch_samples
+
+            # deep copy the model
+            if phase == 'val' and epoch_loss < best_loss:
+                print("saving best model")
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        time_elapsed = time.time() - since
+        print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    print('Best val loss: {:4f}'.format(best_loss))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    # Save the best model weights
+    torch.save(model.state_dict(), 'best_model_big.pth')
+    print('Model saved as best_model_big.pth')
+
+    return model
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+num_class = 1
+model = ResNetUNet(num_class).to(device)
+
+# freeze backbone layers
+#for l in model.base_layers:
+#    for param in l.parameters():
+#        param.requires_grad = False
+
+optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=30, gamma=0.1)
+
+model = train_model(model, optimizer_ft, exp_lr_scheduler, num_epochs=10)
+
+
