6ffb85508ca22a06ed55bfcd0dfb61f39a594cc0
AmaiaZuri <132892218+AmaiaZuri@users.noreply.github.com>
2024-07-11 19:55:42 +0200

    Update pipeline.py

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -6,7 +6,6 @@ import subprocess
 from torchvision import transforms
 from celldet import CellDetectionMetric
 from import_gan import pretrained_generator, process_image
-from Hover_net.hover_net2.run_tile import command
 from DicDataset import DicDataset
 from config import (path_to_endonuke_data_folder, path_to_Hover_net_run_infer, path_to_Hover_net_weights)
 
